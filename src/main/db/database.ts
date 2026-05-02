c3c2710bf156c0c3bd4b111dd0b7efbe2f4cfc90
giovannistagnaro <giovannistagnaro20@gmail.com>
2026-05-02 02:19:55 -0700

    fix: alias for better sqlite multiple ciphers for compatibility with Drizzle

--- src/main/db/database.ts
+++ src/main/db/database.ts
@@ -1,4 +1,4 @@
-import Database from 'better-sqlite3-multiple-ciphers'
+import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
 import { app } from 'electron'
@@ -41,7 +41,7 @@ export function openDatabase(password: string): DrizzleDB {
   }
 }
 
-export function closeDatabase(db: Database.Database): void {
-  db.close()
+export function closeDatabase(db: DrizzleDB): void {
+  db.$client.close()
   log.info('DB Closed')
 }
